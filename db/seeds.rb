d17e4afbe3487439a7e0de819fc2501627abe402
Rubiromi <hiromi.deyoung@gmail.com>
2014-06-12 10:12:58 -0600

    added seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Product.create(name: 'DPL Shirt', price: 14.99, sku: 'dp123', description: 'An awesome purple shirt with the DPL logo')
+Product.create(name: 'DPL Mug', price: 3.25, sku: 'dp124', description: 'An awesome purple mug with the DPL logo')
